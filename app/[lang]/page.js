9068adfe0e42d7f5738a5c113542dcf8dcc2d85b
Jisun696 <istiakkashem35@gmail.com>
2025-09-14 03:47:57 +0600

    refactor

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -4,5 +4,6 @@ export default async function Home({ params: { lang } }) {
   const response = await fetch("http://localhost:3000/api/photos");
   const photos = await response.json();
 
+  console.log(photos);
   return <PhotoList photos={photos} />;
 }
